b4b494de8ad0cb1b5c2f1ffca2a892e2a2e0dbb2
guillaume auriffeuille <auriff.g@gmail.com>
2021-12-25 16:21:44 +0100

    saving scores, adding memory game page

---
+++ memory-front/index.js
@@ -0,0 +1,23 @@
+/* this function will handle events made on index.html when page has been fully loaded
+ two functions will be there:
+    -one for saving the username 
+    -one for displaying scores
+*/
+document.addEventListener('DOMContentLoaded', () => {
+
+    // function called by the input type on the form 
+    function handleSubmit () {
+    
+        // submit pseudo inside of textbox
+        const name = document.getElementById('name').value;  
+        // store pseudo for the session
+        sessionStorage.setItem("NAME", name);
+        return;
+    }
+
+
+    function displayScores() {
+        
+    }
+
+});
